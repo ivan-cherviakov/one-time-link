fa218a54add2c519b80f7bb336bd5f3c971a51f8
ivan cherviakov <cherviakov.ivan@gmail.com>
2024-06-28 21:17:41 +0700

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 import { AppConfig } from './app.config'
@@ -8,8 +9,19 @@ import { AppConfig } from './app.config'
 async function bootstrap() {
   const logger = new Logger(bootstrap.name)
   const config = new ConfigService<AppConfig>(AppConfig)
+
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('One time link')
+    .setDescription('API for one time links')
+    .setVersion('1.0')
+    .addTag('one-time-link')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('swagger', app, swaggerDocument)
+
   await app.listen(config.get('PORT'))
   logger.debug(`server listening on ${config.get('PORT')}`)
 }
